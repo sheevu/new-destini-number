3371b6c23abb64c0d52a16339dc4a67917a21d75
Sheevum Goel <126774066+sheevu@users.noreply.github.com>
2026-07-25 03:31:50 +0530

    Fix preview styling pipeline

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './index.tsx', './App.tsx', './components/**/*.{ts,tsx}', './pages/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
